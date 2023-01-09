4f70a53616231a0e1a6dd0ccee0bf385ce25d32a
David Grisham <dmgrisham@gmail.com>
2023-01-09 12:30:51 -0700

    add some things; update gitignore; remove sv pid files

---
+++ bin/get_brave_history.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+url=$(sqlite3 -separator $'\t' 'file:'"$HOME/.config/BraveSoftware/Brave-Browser/Default/History"'?immutable=1' "$(cat <<EOF
+    select SUBSTR(title,1,150),url from urls order by last_visit_time desc limit 1000;
+EOF
+)" | column --output-separator $'\t' -t -s $'\t' | dmenu -F -i -l 25 | awk -F'\t' '{print $(NF)}')
+[[ -z "$url" ]] && exit 1
+echo "$url"
