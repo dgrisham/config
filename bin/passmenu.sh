6db1d12cd73ef2126f81d173a302eed7cf361801
David Grisham <david.grisham@pluto.tv>
2023-01-26 10:32:39 -0700

    passmenu: add option to look up username

--- bin/passmenu.sh
+++ bin/passmenu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pass_type=$(printf "password\notp\n" | dmenu -p 'type: ')
+pass_type=$(printf "password\notp\nusername" | dmenu -p 'type: ')
 
 case $pass_type in
     password)
@@ -12,5 +12,12 @@ case $pass_type in
         otp_pass=$(pass otp show "$entry")
         [[ -z "$otp_pass" ]] && { DISPLAY=:0 notify-send "otp entry not found" "$entry" ; exit 1 ; }
         xdotool type "$otp_pass"
+        ;;
+    username)
+        entry=$(find $HOME/.password-store -name '*.gpg' -printf '%P\n' | sed 's|.gpg||' | sort | dmenu -F)
+        [[ -z "$entry" ]] && exit 1
+        username=$(pass show "$entry" | grep -oP '\K^login: \K.*?$')
+        [[ -z "$username" ]] && { DISPLAY=:0 notify-send "username not found" "$entry" ; exit 1 ; }
+        xdotool type "$username"
         ;;
 esac
